8a568a58539ca526ca9dca801d00fb95233423d0
ans <elaytpatters@gmail.com>
2023-01-12 22:46:33 +0500

    .

--- index.js
+++ index.js
@@ -120,20 +120,12 @@ let DATA = {
 	uDay: uCDay,
 
 	date: new Date().toLocaleString("uz", {
-		// weekday: "long",
-		// month: "long",
 		day: "numeric",
 		hour: "numeric",
 		minute: "numeric",
-		// timeZoneName: "short",
-		timeZone: "Asia/Tashkent",N
+		timeZone: "Asia/Tashkent",
 	}),
 };
-/**
- * A - We open 'main.mustache'
- * B - We ask Mustache to render our file with the data
- * C - We create a README.md file with the generated output
- */
 
 function generateReadMe() {
 	fs.readFile(MUSTACHE_LIGHT_DIR, (err, data) => {
