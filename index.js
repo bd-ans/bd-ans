593f96ede8258f0b73412bbedd93c0f911335a71
ans <elaytpatters@gmail.com>
2023-01-14 04:06:46 +0500

    .

--- index.js
+++ index.js
@@ -100,15 +100,14 @@ const calage = () => {
 
 
 function generateUUID() {
-	// Public Domain/MIT
-	var d = new Date().getTime(); //Timestamp
+	var d = new Date().getTime();
 	var d2 =
 		(typeof performance !== "undefined" &&
 			performance.now &&
 			performance.now() * 1000) ||
-		0; //Time in microseconds since page-load or 0 if unsupported
+		0;
 	return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-		var r = Math.random() * 16; //random number between 0 and 16
+		var r = Math.random() * 16;
 		if (d > 0) {
 			r = (d + r) % 16 | 0;
 			d = Math.floor(d / 16);
